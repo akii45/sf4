61dbc4ccd232bdd1fcd6e1fa4c73ccbbc2fe33a1
Billy-Vivant <billy.vivant@gmail.com>
2018-11-20 10:48:47 +0100

    quete param converter

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/category", name="category")
+     */
+    public function index()
+    {
+        return $this->render('category/index.html.twig', [
+            'controller_name' => 'CategoryController',
+        ]);
+    }
+
+    /**
+     * @Route("/category/{id}", name="category_show")
+     */
+    public function show(Category $category):Response
+    {
+        return $this->render('category/category.html.twig', ['category'=>$category]);
+    }
+}

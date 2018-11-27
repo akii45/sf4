ec3bb1979312cd0241a7e061e1b81eab481fe012
Billy-Vivant <billy.vivant@gmail.com>
2018-11-27 16:21:16 +0100

    Quete symfony form

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -4,32 +4,104 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Category;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\BrowserKit\Request;
 
 
 class BlogController extends AbstractController
 {
     /**
-     * @Route("blog/{slug}",requirements={"slug"="[a-z0-9-]+"}, name="blog")
+     * Show all row from article's entity
+     *
+     * @Route("/", name="blog_index")
+     * @return Response A response instance
      */
-    public function show($slug = "Article sans titre")
+    public function index() : Response
     {
-        $slug = str_replace("-", " ", $slug);
-        $slug = ucwords($slug);
 
-        return $this->render('blog/index.html.twig', ['slug' => $slug]);
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
 
+        if (!$articles) {
+            throw $this->createNotFoundException(
+                'No article found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/index.html.twig',
+            ['articles' => $articles,
+            ]
+        );
     }
 
     /**
-     * @Route ("blog/category/{category}/all", name ="blog_category")
+     * Getting a article with a formatted slug for title
+     *
+     * @param string $slug The slugger
+     *
+     * @Route("/show{slug<^[a-z0-9-]+$>}",
+     *     defaults={"slug" = null},
+     *     name="blog_show")
+     *  @return Response A response instance
      */
-    public function showAllByCategory(Category $category)
-    {
 
-        return $this->render('blog/category.html.twig', ['category' => $category,
-        'articles' => $category->getArticles(),
+    public function show($slug) : Response
+    {
+        if (!$slug) {
+            throw $this
+                ->createNotFoundException('No slug has been sent to find an article in article\'s table.');
+        }
+
+        $slug = preg_replace(
+            '/-/',
+            ' ', ucwords(trim(strip_tags($slug)), "-")
+        );
+
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findOneBy(['title' => mb_strtolower($slug)]);
+
+        if (!$article) {
+            throw $this->createNotFoundException(
+                'No article with '.$slug.' title, found in article\'s table.'
+            );
+        }
+
+        return $this->render(
+            'blog/show.html.twig',
+            [
+                'article' => $article,
+            ]
+        );
+    }
+
+
+    /**
+     * @Route ("blog/category/{category}", name ="blog_show_category")
+     */
+    public function showByCategory(string $category)
+    {
+        $category = $this ->getDoctrine()
+                            ->getRepository(Category::class)
+                            ->findOneBy(['name'=>$category]);
+
+        $articles = $this ->getDoctrine()
+                ->getRepository(Article::class)
+                ->findBy(['category'=> $category->getId()],
+                    ['id'=>'DESC'],
+                    3
+                );
+
+
+        return $this->render('blog/category.html.twig', [
+            'category' => $category,
+            'articles' =>$articles,
+
         ]);
 
     }
--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Category;
+use App\Form\CategoryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class CategoryController extends AbstractController
 {
     /**
-     * @Route("/category", name="category")
+     * @Route("/category123", name="category")
      */
     public function index()
     {
@@ -26,4 +29,33 @@ class CategoryController extends AbstractController
     {
         return $this->render('category/category.html.twig', ['category'=>$category]);
     }
+
+    /**
+     * @Route ("/category" , name = "category_new", methods="Get|POST")
+     */
+
+    public function new(Request $request): Response
+    {
+        $categories = $this->getDoctrine()
+                         ->getRepository(Category::class)
+                         ->findAll();
+
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class,$category);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+           $em = $this->getDoctrine()->getManager();
+           $em->persist($category);
+           $em->flush();
+
+           return $this->redirectToRoute('blog_index');
+        }
+        return $this->render('category/allCategory.html.twig', [  'category' => $category ,
+                                                                        'form'=> $form->createView(),
+                                                                        'categories'=>$categories
+        ]);
+
+    }
+
 }

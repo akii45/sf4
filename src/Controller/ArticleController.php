08bf5cdd059b934f9453c6ded509e7c3075a0ddb
Billy-Vivant <billy.vivant@gmail.com>
2018-11-27 17:37:45 +0100

    ma vie, ma vrai, auchan

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/article", name="article")
+     */
+    public function index()
+    {
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+    /**
+     * @Route("/article/new", name="article_new")
+     */
+    public function add(Request $request): Response
+    {
+
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class,$article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('blog_index');
+        }
+        return $this->render('article/addArticle.html.twig', [  'article' => $article ,
+            'form'=> $form->createView(),
+        ]);
+
+    }
+
+
+}
